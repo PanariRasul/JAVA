f264361ff901c5269cb54b816152159eb57a46f3
PanariRasul <panarirasul@gmail.com>
2024-01-04 01:46:14 +0530

    switch statement

---
+++ Shiftoperater/src/SwitchStatement.java
@@ -0,0 +1,20 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int num=2;
+
+        switch (num){
+            case 0:
+                System.out.println("Number is Zero");
+                break;
+            case 1:
+                System.out.println("Number Is One");
+                break;
+            case 2:
+                System.out.println("Number Is Two");
+                break;
+            case 3:
+                System.out.println("Number is Three");
+                break;
+        }
+    }
+}
