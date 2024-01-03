cf0fe878ae37ec4e1a589908b0802ac428cb829c
PanariRasul <panarirasul@gmail.com>
2024-01-04 02:07:36 +0530

    do while loop

--- Shiftoperater/src/WhileLoop.java
+++ Shiftoperater/src/WhileLoop.java
@@ -8,5 +8,14 @@ public class WhileLoop {
             System.out.println(i+" Hello World!");
             i++;
         }
+        System.out.println("==========================================================================");
+
+        int j=25;
+        do{
+            System.out.println(j+" Hello Motrola");
+            j++;
+        }while (j<=15);
     }
+
+
 }
