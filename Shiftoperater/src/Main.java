d55ffdba8b06d910c502782a3e60e12011149203
PanariRasul <panarirasul@gmail.com>
2024-01-03 17:50:39 +0530

    java shift operater

---
+++ Shiftoperater/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int a=4;
+        int n=2;
+        int r=a<<n;
+        int s=a>>n;
+        System.out.println("Right Shift"+s);
+        System.out.println("Left Shift"+r);
+    }
+}
